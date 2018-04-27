c3e1dd2c256bff4951ec95ca3f7f97ea51067303
tyuownu <tyuownu@gmail.com>
2018-04-27 11:22:53 +0800

    docs(update download script): adding navigation package

--- docs/scripts/download.sh
+++ docs/scripts/download.sh
@@ -17,21 +17,21 @@ wget https://github.com/tyuownu/ceres-solver/archive/${TAG}.zip \
 wget https://github.com/tyuownu/stage_ros/archive/${TAG}.zip \
     -O stage_ros.zip
 
-#wget https://github.com/tyuownu/navigation/archive/${TAG}.zip \
-#    -O navigation.zip
+wget https://github.com/tyuownu/navigation/archive/${TAG}.zip \
+    -O navigation.zip
 
 unzip -n cartographer.zip -d ../../../
 unzip -n cartographer_ros.zip -d ../../../
 unzip -n cartographer_turtlebot.zip -d ../../../
 unzip -n ceres-solver.zip -d ../../../
 unzip -n stage_ros.zip -d ../../../
-# unzip -n navigation.zip -d ../../../
+unzip -n navigation.zip -d ../../../
 
 cd ../../../
 
-mv cartographer-neoware-0.1.0 cartographer
-mv cartographer_ros-neoware-0.1.0 cartographer_ros
-mv cartographer_turtlebot-neoware-0.1.0 cartographer_turtlebot
-mv ceres-solver-neoware-0.1.0 ceres-solver
-mv stage_ros-neoware-0.1.0 stage_ros
-# mv navigation-neoware-0.1.0 navigation
+mv cartographer-${TAG} cartographer
+mv cartographer_ros-${TAG} cartographer_ros
+mv cartographer_turtlebot-${TAG} cartographer_turtlebot
+mv ceres-solver-${TAG} ceres-solver
+mv stage_ros-${TAG} stage_ros
+mv navigation-${TAG} navigation
