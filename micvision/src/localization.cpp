37a318c18c43de9a8b61023cc3409d73921807f6
Ajay Vishnu <ajay.vishnu@gmail.com>
2020-04-03 12:03:37 +0530

    Update CMakeLists to build headers first (#11)
    * Update CMakeLists to build headers first
    
    Currently, message headers won't always get built first causing catkin make to fail on a fresh build. 
    Ref: https://answers.ros.org/question/188982/message-headers-wont-build-first/
    
    * Update localization.cpp

--- micvision/src/localization.cpp
+++ micvision/src/localization.cpp
@@ -248,7 +248,7 @@ void MicvisionLocalization::scoreLaserScanSamples() {
           }
 
           score = sample_score;
-          best_angle_ = -M_PI + i * laserscan_anglar_step_ * RADIAN_PRE_DEGREE;
+          best_angle_ = i * laserscan_anglar_step_ * RADIAN_PRE_DEGREE;
           best_position_ = Pixel(u, v);
         }
       }
